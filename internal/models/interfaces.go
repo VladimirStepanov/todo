f368d210224987480c97e7be96316d83e321f1af
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-21 23:32:24 +0300

    Add Confirm method to UserService and UserRepository

--- internal/models/interfaces.go
+++ internal/models/interfaces.go
@@ -2,10 +2,12 @@ package models
 
 type UserService interface {
 	Create(Email, Password string) (*User, error)
+	ConfirmEmail(Link string) error
 }
 
 type UserRepository interface {
 	Create(user *User) (*User, error)
+	ConfirmEmail(Link string) error
 }
 
 type MailService interface {
