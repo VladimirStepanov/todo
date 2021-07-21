b293f6cc63a2ac235b69c9d747a39426c82aa610
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-21 23:35:20 +0300

    Update mocks

--- internal/models/mocks/userrepo_mock.go
+++ internal/models/mocks/userrepo_mock.go
@@ -12,6 +12,20 @@ type UserRepository struct {
 	mock.Mock
 }
 
+// ConfirmEmail provides a mock function with given fields: Link
+func (_m *UserRepository) ConfirmEmail(Link string) error {
+	ret := _m.Called(Link)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string) error); ok {
+		r0 = rf(Link)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // Create provides a mock function with given fields: user
 func (_m *UserRepository) Create(user *models.User) (*models.User, error) {
 	ret := _m.Called(user)
--- internal/models/mocks/userservice_mock.go
+++ internal/models/mocks/userservice_mock.go
@@ -12,6 +12,20 @@ type UserService struct {
 	mock.Mock
 }
 
+// ConfirmEmail provides a mock function with given fields: Link
+func (_m *UserService) ConfirmEmail(Link string) error {
+	ret := _m.Called(Link)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(string) error); ok {
+		r0 = rf(Link)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
 // Create provides a mock function with given fields: Email, Password
 func (_m *UserService) Create(Email string, Password string) (*models.User, error) {
 	ret := _m.Called(Email, Password)
