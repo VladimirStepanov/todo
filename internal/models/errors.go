b289f81849ddde383ebc3232f8238370c2ac5d51
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-26 23:35:09 +0300

    Add error ErrUserNotFound

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrUserAlreadyExists    = errors.New("user already exists")
 	ErrConfirmLinkNotExists = errors.New("confirm link not found")
+	ErrUserNotFound         = errors.New("user not found")
 )
