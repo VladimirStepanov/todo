3652f34a98203b6f4a722900b2726de8be8fd9d0
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-28 16:25:57 +0300

    Add new error ErrMaxLoggedIn

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -7,4 +7,5 @@ var (
 	ErrConfirmLinkNotExists = errors.New("confirm link not found")
 	ErrBadUser              = errors.New("invalid email or password")
 	ErrUserNotActivated     = errors.New("user is not activated")
+	ErrMaxLoggedIn          = errors.New("maximum logged in users")
 )
