31be8094fc587ca65f010e5ddf88091b29b68dda
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-28 20:05:10 +0300

    Implenent and test signIn handler

--- internal/handler/sign-in.go
+++ internal/handler/sign-in.go
@@ -3,9 +3,52 @@ package handler
 import (
 	"net/http"
 
+	"github.com/VladimirStepanov/todo-app/internal/models"
 	"github.com/gin-gonic/gin"
 )
 
 func (h *Handler) signIn(c *gin.Context) {
-	c.String(http.StatusOK, "Sign in")
+	var req signupReq
+	if !bindData(c, &req) {
+		return
+	}
+
+	user, err := h.UserService.SignIn(req.Email, req.Password)
+
+	if err != nil {
+		switch err {
+		case models.ErrBadUser:
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":  "error",
+				"message": err.Error(),
+			})
+		case models.ErrUserNotActivated:
+			c.JSON(http.StatusForbidden, gin.H{
+				"status":  "error",
+				"message": err.Error(),
+			})
+		default:
+			h.InternalError(c, err)
+		}
+		return
+	}
+
+	td, err := h.TokenService.NewTokenPair(user.ID)
+	if err != nil {
+		switch err {
+		case models.ErrMaxLoggedIn:
+			c.JSON(http.StatusForbidden, gin.H{
+				"status":  "error",
+				"message": err.Error(),
+			})
+		default:
+			h.InternalError(c, err)
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"access_token":  td.AccessToken,
+		"refresh_token": td.RefreshToken,
+	})
 }
---
+++ internal/handler/sign-in_test.go
@@ -0,0 +1,125 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/VladimirStepanov/todo-app/internal/models"
+	"github.com/VladimirStepanov/todo-app/internal/models/mocks"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSignIn(t *testing.T) {
+	retUser := &models.User{ID: 1}
+
+	tests := []struct {
+		name      string
+		usRetUser *models.User
+		usRetErr  error
+		tsRetErr  error
+		tsRetTd   *models.TokenDetails
+		code      int
+		errMsg    string
+	}{
+		{
+			name:      "Internal error for SignIn",
+			usRetUser: nil,
+			usRetErr:  ErrUnknown,
+			tsRetErr:  nil,
+			tsRetTd:   nil,
+			code:      http.StatusInternalServerError,
+			errMsg:    "Internal server error",
+		},
+		{
+			name:      "Not activated user error",
+			usRetUser: nil,
+			usRetErr:  models.ErrUserNotActivated,
+			tsRetErr:  nil,
+			tsRetTd:   nil,
+			code:      http.StatusForbidden,
+			errMsg:    models.ErrUserNotActivated.Error(),
+		},
+		{
+			name:      "User not found",
+			usRetUser: nil,
+			usRetErr:  models.ErrBadUser,
+			tsRetErr:  nil,
+			tsRetTd:   nil,
+			code:      http.StatusNotFound,
+			errMsg:    models.ErrBadUser.Error(),
+		},
+		{
+			name:      "Internal error for NewTokenPair",
+			usRetUser: retUser,
+			usRetErr:  nil,
+			tsRetErr:  ErrUnknown,
+			tsRetTd:   nil,
+			code:      http.StatusInternalServerError,
+			errMsg:    "Internal server error",
+		},
+		{
+			name:      "Max user logged in error",
+			usRetUser: retUser,
+			usRetErr:  nil,
+			tsRetErr:  models.ErrMaxLoggedIn,
+			tsRetTd:   nil,
+			code:      http.StatusForbidden,
+			errMsg:    models.ErrMaxLoggedIn.Error(),
+		},
+		{
+			name:      "Success",
+			usRetUser: retUser,
+			usRetErr:  nil,
+			tsRetErr:  nil,
+			tsRetTd: &models.TokenDetails{
+				AccessToken:  "access_token",
+				RefreshToken: "refresh_token",
+				UUID:         "",
+			},
+			code:   http.StatusOK,
+			errMsg: "",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			reqData := `{"email": "test@test.com", "password": "123456789"}`
+			usObj := new(mocks.UserService)
+			usObj.On("SignIn", mock.Anything, mock.Anything).Return(tc.usRetUser, tc.usRetErr)
+			tsObj := new(mocks.TokenService)
+			tsObj.On("NewTokenPair", mock.Anything).Return(tc.tsRetTd, tc.tsRetErr)
+
+			handler := New(usObj, nil, tsObj, getTestLogger())
+			r := handler.InitRoutes(gin.TestMode)
+			req := httptest.NewRequest(http.MethodPost, "/auth/sign-in", bytes.NewBuffer([]byte(reqData)))
+			req.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			res := w.Result()
+
+			defer res.Body.Close()
+
+			require.Equalf(t, tc.code, res.StatusCode, "Expected sc: %d, got: %d\n", tc.code, res.StatusCode)
+			actResp := map[string]interface{}{}
+
+			data, err := ioutil.ReadAll(res.Body)
+			require.NoErrorf(t, err, "Error while ReadAll %v", err)
+			err = json.Unmarshal(data, &actResp)
+			require.NoError(t, err)
+			if tc.code != 200 {
+				require.Equal(t, actResp["status"], "error")
+				require.Equal(t, actResp["message"], tc.errMsg)
+			} else {
+				require.NotEmpty(t, actResp["access_token"])
+				require.NotEmpty(t, actResp["refresh_token"])
+			}
+		})
+	}
+}
