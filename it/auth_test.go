5506f96a1c5db994cb25045d44ba3fb81f04c520
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-31 14:13:00 +0300

    Change local makeRequest method to helpers.MakeRequest function

--- it/auth_test.go
+++ it/auth_test.go
@@ -4,31 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/VladimirStepanov/todo-app/internal/handler"
+	"github.com/VladimirStepanov/todo-app/internal/helpers"
 	"github.com/VladimirStepanov/todo-app/internal/models"
 	"github.com/stretchr/testify/require"
 )
-
-func (suite *TestingSuite) makeRequest(method, path string, input *bytes.Buffer) (int, []byte) {
-	req := httptest.NewRequest(method, path, input)
-	req.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	suite.router.ServeHTTP(w, req)
-
-	res := w.Result()
-
-	defer res.Body.Close()
-
-	data, err := ioutil.ReadAll(res.Body)
-	require.NoError(suite.T(), err)
-
-	return res.StatusCode, data
-}
 
 func (suite *TestingSuite) TestSignUp() {
 
@@ -54,7 +37,13 @@ func (suite *TestingSuite) TestSignUp() {
 
 	for _, tc := range tests {
 		suite.T().Run(tc.name, func(t *testing.T) {
-			code, data := suite.makeRequest(http.MethodPost, "/auth/sign-up", bytes.NewBuffer([]byte(tc.data)))
+			code, data := helpers.MakeRequest(
+				suite.router,
+				suite.T(),
+				http.MethodPost,
+				"/auth/sign-up",
+				bytes.NewBuffer([]byte(tc.data)),
+			)
 			require.Equal(t, tc.code, code)
 			if tc.errMsg != "" {
 				resp := handler.ErrorResponse{}
@@ -96,7 +85,13 @@ func (suite *TestingSuite) TestEmailConfirmation() {
 
 	for _, tc := range tests {
 		suite.T().Run(tc.name, func(t *testing.T) {
-			code, data := suite.makeRequest(http.MethodGet, fmt.Sprintf("/auth/confirm/%s", tc.link), bytes.NewBuffer([]byte{}))
+			code, data := helpers.MakeRequest(
+				suite.router,
+				suite.T(),
+				http.MethodGet,
+				fmt.Sprintf("/auth/confirm/%s", tc.link),
+				bytes.NewBuffer([]byte{}),
+			)
 			require.Equal(t, tc.code, code)
 			if tc.errMsg != "" && tc.code != http.StatusOK {
 				resp := handler.ErrorResponse{}
@@ -115,7 +110,13 @@ func (suite *TestingSuite) TestSignIn() {
 	input := fmt.Sprintf(`{"email": "%s", "password": "%s"}`, maxLoggedInUser.Email, defaultPassword)
 
 	for i := 0; i < maxLoggenInCount; i++ {
-		code, _ := suite.makeRequest(http.MethodPost, "/auth/sign-in", bytes.NewBuffer([]byte(input)))
+		code, _ := helpers.MakeRequest(
+			suite.router,
+			suite.T(),
+			http.MethodPost,
+			"/auth/sign-in",
+			bytes.NewBuffer([]byte(input)),
+		)
 		require.Equal(suite.T(), http.StatusOK, code)
 	}
 
@@ -153,7 +154,13 @@ func (suite *TestingSuite) TestSignIn() {
 
 	for _, tc := range tests {
 		suite.T().Run(tc.name, func(t *testing.T) {
-			code, data := suite.makeRequest(http.MethodPost, "/auth/sign-in", bytes.NewBuffer([]byte(tc.input)))
+			code, data := helpers.MakeRequest(
+				suite.router,
+				suite.T(),
+				http.MethodPost,
+				"/auth/sign-in",
+				bytes.NewBuffer([]byte(tc.input)),
+			)
 			require.Equal(t, tc.code, code)
 			if tc.errMsg != "" && tc.code != http.StatusOK {
 				resp := handler.ErrorResponse{}
@@ -161,7 +168,6 @@ func (suite *TestingSuite) TestSignIn() {
 				require.NoError(t, err)
 				require.Equal(t, "error", resp.Status)
 				require.Equal(t, tc.errMsg, resp.Message)
-
 			}
 		})
 	}
