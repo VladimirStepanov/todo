169af2ffc64aa4f46165e8040cadee562aa7653b
Vladimir Stepanov <vovakirishi@gmail.com>
2021-07-31 13:21:30 +0300

    Add ErrBadToken, ErrUserUnauthorized, ErrTokenExpired errors

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -8,4 +8,7 @@ var (
 	ErrBadUser              = errors.New("invalid email or password")
 	ErrUserNotActivated     = errors.New("user is not activated")
 	ErrMaxLoggedIn          = errors.New("maximum logged in users")
+	ErrBadToken             = errors.New("invalid token")
+	ErrUserUnauthorized     = errors.New("user is unauthorized")
+	ErrTokenExpired         = errors.New("token is expired")
 )
