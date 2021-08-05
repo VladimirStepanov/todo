2847abcaa6acea36b14259b936a8c6f29fd47d66
Vladimir Stepanov <vovakirishi@gmail.com>
2021-08-05 18:33:48 +0300

    Implement onlyAdminAccess middleware

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/VladimirStepanov/todo-app/internal/models"
@@ -58,5 +59,47 @@ func (h *Handler) authMiddleware(c *gin.Context) {
 
 	c.Set(idCtx, userID)
 	c.Set(CtxUUID, userUUID)
+	c.Next()
+}
+
+func (h *Handler) onlyAdminAccess(c *gin.Context) {
+	userID, err := h.GetUserId(c)
+	if err != nil {
+		h.InternalError(c, err)
+		c.Abort()
+		return
+	}
+
+	listID, err := strconv.ParseInt(c.Param("list_id"), 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": models.ErrBadParam.Error(),
+		})
+		c.Abort()
+		return
+	}
+
+	err = h.ListService.IsListAdmin(userID, listID)
+
+	if err != nil {
+		switch err {
+		case models.ErrNoList:
+			c.JSON(http.StatusNotFound, gin.H{
+				"status":  "error",
+				"message": err.Error(),
+			})
+		case models.ErrNoListAccess:
+			c.JSON(http.StatusForbidden, gin.H{
+				"status":  "error",
+				"message": err.Error(),
+			})
+		default:
+			h.InternalError(c, err)
+		}
+		c.Abort()
+		return
+	}
+
 	c.Next()
 }
