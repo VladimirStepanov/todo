6edef93dc61eae8f3c64a6d13cfc7c79e1ac0d90
noname <noname@anb.lan>
2021-09-04 18:21:18 +0300

    Create Item and UpdateItemReq structs for models

---
+++ internal/models/item.go
@@ -0,0 +1,15 @@
+package models
+
+type Item struct {
+	ID          int64  `json:"id" db:"id"`
+	ListID      int64  `json:"list_id" db:"list_id"`
+	Title       string `json:"title" db:"title"`
+	Description string `json:"description" db:"description"`
+	Done        bool   `json:"done"  db:"done"`
+}
+
+type UpdateItemReq struct {
+	Title       *string `json:"title"`
+	Description *string `json:"description"`
+	Done        *bool   `json:"done"`
+}
