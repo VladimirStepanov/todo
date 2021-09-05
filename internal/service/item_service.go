2ec9c011e4ec724ba6b6403bb8c5a8ccc7caee89
noname <noname@anb.lan>
2021-09-05 22:02:37 +0300

    Create ItemService struct with stubs

---
+++ internal/service/item_service.go
@@ -0,0 +1,37 @@
+package service
+
+import "github.com/VladimirStepanov/todo-app/internal/models"
+
+type ItemService struct {
+	repo models.ItemRepository
+}
+
+func NewItemService(repo models.ItemRepository) models.ItemService {
+	return &ItemService{
+		repo: repo,
+	}
+}
+
+func (is *ItemService) Create(title, description string, listID int64) (int64, error) {
+	return 0, nil
+}
+
+func (is *ItemService) GetItems(listID int64) ([]*models.Item, error) {
+	return nil, nil
+}
+
+func (is *ItemService) GetItemBydID(listID, itemID int64) (*models.Item, error) {
+	return nil, nil
+}
+
+func (is *ItemService) Update(listID, itemID int64, item *models.UpdateItemReq) error {
+	return nil
+}
+
+func (is *ItemService) Done(listID, itemID int64) error {
+	return nil
+}
+
+func (is *ItemService) Delete(listID, itemID int64) error {
+	return nil
+}
