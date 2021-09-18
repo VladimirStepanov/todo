cfefe709e9fc7d99451ceaf6182064dab4f6d713
noname <noname@anb.lan>
2021-09-19 00:25:21 +0300

    Add swagger comments to item handlers

--- internal/handler/items.go
+++ internal/handler/items.go
@@ -13,6 +13,21 @@ type itemCreateReq struct {
 	Description string `json:"description" binding:"required"`
 }
 
+// CreateItem godoc
+// @Summary Create item
+// @Tags items
+// @Accept  json
+// @Produce  json
+// @ID create-item
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param input body itemCreateReq true "item input"
+// @Success 200 {object} ItemCreateResponse "success item creation"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/items [post]
 func (h *Handler) itemCreate(c *gin.Context) {
 
 	var req itemCreateReq
@@ -34,6 +49,20 @@ func (h *Handler) itemCreate(c *gin.Context) {
 	})
 }
 
+// GetItems godoc
+// @Summary Get items
+// @Tags items
+// @Accept  json
+// @Produce  json
+// @ID get-items
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Success 200 {object} UserItemsResponse "all user items"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/items [get]
 func (h *Handler) getItems(c *gin.Context) {
 	listID, _ := strconv.ParseInt(c.Param("list_id"), 10, 64)
 
@@ -48,6 +77,21 @@ func (h *Handler) getItems(c *gin.Context) {
 
 }
 
+// GetItem godoc
+// @Summary Get item
+// @Tags items
+// @Accept  json
+// @Produce  json
+// @ID get-item
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param item_id path int true "item_id"
+// @Success 200 {object} models.Item "item"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found, item not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/items/{item_id} [get]
 func (h *Handler) getItemByID(c *gin.Context) {
 
 	listID, _ := strconv.ParseInt(c.Param("list_id"), 10, 64)
@@ -80,6 +124,22 @@ func (h *Handler) getItemByID(c *gin.Context) {
 
 }
 
+// UpdateItem godoc
+// @Summary Update item
+// @Tags items
+// @Accept  json
+// @Produce  json
+// @ID update-item
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param item_id path int true "item_id"
+// @Param input body models.UpdateItemReq true "input"
+// @Success 200 {string} status	"success"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found, item not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/items/{item_id} [patch]
 func (h *Handler) updateItem(c *gin.Context) {
 	listID, _ := strconv.ParseInt(c.Param("list_id"), 10, 64)
 	itemID, err := strconv.ParseInt(c.Param("item_id"), 10, 64)
@@ -124,6 +184,21 @@ func (h *Handler) updateItem(c *gin.Context) {
 
 }
 
+// DoneItem godoc
+// @Summary Done item
+// @Tags items
+// @Accept  json
+// @Produce  json
+// @ID done-item
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param item_id path int true "item_id"
+// @Success 200 {string} status	"success"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found, item not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/items/{item_id}/done [patch]
 func (h *Handler) doneItem(c *gin.Context) {
 	listID, _ := strconv.ParseInt(c.Param("list_id"), 10, 64)
 	itemID, err := strconv.ParseInt(c.Param("item_id"), 10, 64)
@@ -162,6 +237,21 @@ func (h *Handler) doneItem(c *gin.Context) {
 
 }
 
+// DeleteItem godoc
+// @Summary Delete item
+// @Tags items
+// @Accept  json
+// @Produce  json
+// @ID delete-item
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param item_id path int true "item_id"
+// @Success 200 {string} status	"success"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found, item not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/items/{item_id} [delete]
 func (h *Handler) deleteItem(c *gin.Context) {
 	listID, _ := strconv.ParseInt(c.Param("list_id"), 10, 64)
 	itemID, err := strconv.ParseInt(c.Param("item_id"), 10, 64)
