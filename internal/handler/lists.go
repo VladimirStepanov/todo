54d12a8ba5183f9c6c1faa720b6b06c8b5951cd9
noname <noname@anb.lan>
2021-09-19 00:25:00 +0300

    Add swagger comments to list handlers

--- internal/handler/lists.go
+++ internal/handler/lists.go
@@ -13,6 +13,20 @@ type listCreateReq struct {
 	Description string `json:"description" binding:"required"`
 }
 
+// CreateList godoc
+// @Summary Create list
+// @Tags lists
+// @Accept  json
+// @Produce  json
+// @ID create-list
+// @Security ApiKeyAuth
+// @Param input body listCreateReq true "list input"
+// @Success 200 {object} ListCreateResponse "success list creation"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists [post]
 func (h *Handler) listCreate(c *gin.Context) {
 	var req listCreateReq
 	if ok := bindData(c, &req); !ok {
@@ -35,6 +49,19 @@ func (h *Handler) listCreate(c *gin.Context) {
 	c.JSON(http.StatusOK, &ListCreateResponse{"success", listID})
 }
 
+// GetList godoc
+// @Summary Get list by id
+// @Tags lists
+// @Produce  json
+// @ID get-list
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Success 200 {object} models.List "list"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "list not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id} [get]
 func (h *Handler) getListByID(c *gin.Context) {
 	userID, err := h.GetUserId(c)
 	if err != nil {
@@ -73,6 +100,22 @@ type editRoleReq struct {
 	IsAdmin *bool `json:"is_admin" binding:"required"`
 }
 
+// EditRole godoc
+// @Summary Edit user role for list
+// @Tags lists
+// @Accept  json
+// @Produce  json
+// @ID edit-role
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param input body editRoleReq true "edit-role input"
+// @Success 200 {string} status	"success"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 403 {object} ErrorResponse "current user is not admin"
+// @Failure 404 {object} ErrorResponse "list not found, user not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id}/edit-role [patch]
 func (h *Handler) editRole(c *gin.Context) {
 	var req editRoleReq
 	if ok := bindData(c, &req); !ok {
@@ -99,6 +142,20 @@ func (h *Handler) editRole(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
+// DeleteList godoc
+// @Summary Delete list by id
+// @Tags lists
+// @Produce  json
+// @ID delete-list
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Success 200 {string} status	"success"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 403 {object} ErrorResponse "current user is not admin"
+// @Failure 404 {object} ErrorResponse "list not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id} [delete]
 func (h *Handler) deleteList(c *gin.Context) {
 	listID, _ := strconv.ParseInt(c.Param("list_id"), 10, 64)
 
@@ -120,6 +177,21 @@ func (h *Handler) deleteList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
+// UpdateList godoc
+// @Summary Update list by id
+// @Tags lists
+// @Produce  json
+// @ID delete-list
+// @Security ApiKeyAuth
+// @Param list_id path int true "list_id"
+// @Param input body models.UpdateListReq true "input"
+// @Success 200 {string} status	"success"
+// @Failure 400 {object} ErrorResponse	"bad input, auth header errors"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 403 {object} ErrorResponse "current user is not admin"
+// @Failure 404 {object} ErrorResponse "list not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists/{list_id} [patch]
 func (h *Handler) updateList(c *gin.Context) {
 
 	var req models.UpdateListReq
@@ -152,6 +224,17 @@ func (h *Handler) updateList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "success"})
 }
 
+// GetUserLists godoc
+// @Summary Get all user lists
+// @Tags lists
+// @Produce  json
+// @ID get-lists
+// @Security ApiKeyAuth
+// @Success 200 {object} UserListsResponse "lists"
+// @Failure 401 {object} ErrorResponse "user is not authorized"
+// @Failure 404 {object} ErrorResponse "user not found"
+// @Failure 500 {object} ErrorResponse "internal error"
+// @Router /api/lists [get]
 func (h *Handler) getUserLists(c *gin.Context) {
 	result, err := h.ListService.GetUserLists(c.GetInt64(idCtx))
 
@@ -160,5 +243,5 @@ func (h *Handler) getUserLists(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"status": "success", "result": result})
+	c.JSON(http.StatusOK, UserListsResponse{"success", result})
 }
